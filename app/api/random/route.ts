f6030448cd07c633a483231b96520315d8d94612
Richard Jean <Richinbk1@gmail.com>
2024-04-01 14:09:44 -0400

    created new random route

---
+++ app/api/random/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse, NextRequest } from "next/server";
+import mongoose from "mongoose";
+import { Quotes } from "../Schemas";
+import { isDynamicServerError } from "next/dist/client/components/hooks-server-context";
+
+
+
+export async function GET(request: NextRequest){
+    try {
+        let mongo_uri:string | undefined = process.env.NEXT_PUBLIC_MONGO_URI;
+
+        if(mongo_uri != undefined){
+            await mongoose.connect(mongo_uri,{
+                dbName:'Finance-Quotes',
+            });
+        }
+
+        let query = await Quotes.find({}).then((docs) => {
+
+            let random = Math.floor(Math.random() * docs.length);
+            return docs[random];
+        });
+
+        mongoose.disconnect();
+        return NextResponse.json({query}, {status: 200, headers:{'cache':'no-store'}})
+  
+    } catch (error) {
+  
+        if (isDynamicServerError(error)) {
+            throw error;
+        }
+        console.log(error);
+    }
+}
